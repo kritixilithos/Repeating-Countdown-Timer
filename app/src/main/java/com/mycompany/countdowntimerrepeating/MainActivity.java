73c1d138db16bf6016844001418a89e8955da26f
Mohammed Malik <mohammed@Mohammeds-MacBook-Air.local>
2015-09-30 21:00:58 +0200

    Initial commit

---
+++ app/src/main/java/com/mycompany/countdowntimerrepeating/MainActivity.java
@@ -0,0 +1,93 @@
+package com.mycompany.countdowntimerrepeating;
+
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    TextView tvv;
+    Uri notification;
+    Ringtone r;
+    boolean clicked = false;
+    EditText et;
+
+
+    public void repeatTimer(double minutes) {
+        final double milliseconds = minutes*60000;
+        final double mins = minutes;
+
+        tv = (TextView) findViewById(R.id.tv);
+        new CountDownTimer((int) milliseconds, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+                tv.setText("seconds remaining: " + millisUntilFinished / 1000);
+                if (millisUntilFinished < milliseconds-1000) {
+                    r.stop();
+                }
+            }
+
+            public void onFinish() {
+                r.play();
+
+                repeatTimer(mins);
+            }
+        }.start();
+    }
+
+
+    public void startRepeater(View view) {
+        if(!clicked) {
+            et = (EditText) findViewById(R.id.et);
+            tvv = (TextView) findViewById(R.id.tvv);
+
+            notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+            r.play();
+
+            double numValue = Double.parseDouble(et.getText().toString());
+
+            tvv.setText(numValue + "");
+            repeatTimer(numValue);
+        }
+        clicked = true;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
